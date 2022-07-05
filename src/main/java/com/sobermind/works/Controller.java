82d90dbb32a36d7aea9d67908cdb1237449dd61b
Destiny "David" Gumede <Davidx3D@outlook.com>
2022-07-05 21:55:24 +0200

    Fixed Wireless Not Working
    The Option For Using The Application Wirelessly Didnt Work But Now It Is Fixed

--- src/main/java/com/sobermind/works/Controller.java
+++ src/main/java/com/sobermind/works/Controller.java
@@ -11,33 +11,39 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import com.sobermind.works.Audio;
 
-public class Controller implements Initializable {
+public class Controller implements Initializable{
 
     @FXML private CheckBox offscrn,stay,offcl,showt,fsmode,shortmod;
     @FXML private TextField bitrate,port,devip,maxsize,advpanel;
     @FXML private TextArea devices,output;
     @FXML private Label iplabel,applicable,portlabel;
     @FXML private RadioButton wired,wireless,bt,snd;
+    
     //Button Functions
     @FXML
     void onRunbtn() throws Exception {
-
+    
         boolean Off = offscrn.isSelected();
         boolean On = stay.isSelected();
         boolean Offcl = offcl.isSelected();
         boolean Show = showt.isSelected();
         boolean Fs = fsmode.isSelected();
         boolean Short = shortmod.isSelected();
-
+    
         String Bit = bitrate.getText();
         String Max = maxsize.getText();
         String Adv = advpanel.getText();
         String IP  = devip.getText();
         String Port= port.getText();
-    
+        
         ArrayList<String> cmd = new ArrayList<>();
-    
+        
+        if (IP!="") {
+            cmd.add(" --tcpip="+IP);}
+        else if (Port!="") {
+            cmd.add(" --tcpip="+IP+":"+Port);}
         if(Off) cmd.add(" -S");
         if(On) cmd.add(" -w");
         if(Offcl) cmd.add(" --power-off-on-close");
@@ -53,20 +59,19 @@ public class Controller implements Initializable {
                               .replace("]", "")
                               .trim();
     
-            //Below Is The Important Stuff
-            String mods = Script;
+        //Below Is The Important Stuff
+        String mods = Script;
+        Process p = Runtime.getRuntime().exec("cmd.exe /c scrcpy " + Script + " "+Adv);
+        //Opens Cmd And Runs Scrcpy With Added Scripts
+        String sr = null;
+        BufferedReader rs = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        while ((sr = rs.readLine()) != null) {
 
-                Process p = Runtime.getRuntime().exec("cmd.exe /c scrcpy " + Script + " "+Adv);
-                    //Opens Cmd And Runs Scrcpy With Added Scripts
-                    String sr = null;
-                    BufferedReader rs = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                    while ((sr = rs.readLine()) != null) {
-
-                        PrintStream printStream = new PrintStream(new TextOut(output));
-                        System.setOut(printStream);
-                        System.out.println(sr);//Shows Progress And Potential Errors On a Field
-                        output.setWrapText(true);
-                    }
+            PrintStream printStream = new PrintStream(new TextOut(output));
+            System.setOut(printStream);
+            System.out.println(sr);//Shows Progress And Potential Errors On a Field
+            output.setWrapText(true);
+            }
         }
     @FXML
     protected  void onExitbtn(){
@@ -100,17 +105,21 @@ public class Controller implements Initializable {
                 portlabel.setVisible(true);
         }
     }
+    //The Code Below Uses IDevice To Check If a Device Has Been Connected Or Disconnected From The PC
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        
+ 
         AndroidDebugBridge.init(false);
-        devices.setText("Please Connect An Android Device");
         devices.setEditable(false);
-
+        output.setEditable(false);
+    
         AndroidDebugBridge debugBridge = AndroidDebugBridge.createBridge("adb.exe", true);
         if (debugBridge == null) {
             System.exit(1);
         }
         AndroidDebugBridge.addDeviceChangeListener(new AndroidDebugBridge.IDeviceChangeListener() {
+            
             public void deviceChanged(IDevice device, int arg1) {
                 //Not Necessary For Now
             }
@@ -138,15 +147,10 @@ public class Controller implements Initializable {
             }
             public void deviceDisconnected(IDevice device) {
 
-                String a = ""; //Created To Avoid Empty Character Literal Error
-                devices.setText(a);
-
-                String s = String.format("%s Disconnected", device.getSerialNumber());
-                devices.setText(s);
-
+                devices.setText("");
+                devices.setText(String.format("%s Disconnected", device.getSerialNumber()));
                 try {
                     String ss = null;
-
                     Process p = Runtime.getRuntime().exec("cmd.exe /c adb devices");
                     BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
@@ -163,9 +167,7 @@ public class Controller implements Initializable {
                 }
             }
         });
-        output.setEditable(false);
         wired.setSelected(true);
-
         applicable.setVisible(false);
         devip.setVisible(false);
         iplabel.setVisible(false);
